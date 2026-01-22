d3c48f887671b49a3bd0cc464881d6a2c0aabf59
AtharvDharwat <atharvmdharwat@gmail.com>
2026-01-22 09:20:01 +0530

    Implement binary search algorithm in C

---
+++ binarysearch.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, target, low, high, mid, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d sorted elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {-
+        mid = (low + high) / 2;
+
+        if (arr[mid] == target) {
+            printf("Found at index: %d\n", mid);
+            found = 1;
+            break; 
+        }
+
+        if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (found == 0) {
+        printf("Not found\n");
+    }
+
+    return 0;
+}
